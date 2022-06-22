3f13de0dff02e26fd6be57a49af674c2970fc354
PrinceTD <prince.talukder001@gmail.com>
2022-06-22 16:23:23 +0600

    first commit

---
+++ src/Componets/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../Header/Navbar'
+import './Home.css'
+
+export default function Home() {
+  return (
+    <div>
+        <Navbar></Navbar>
+    </div>
+  )
+}
