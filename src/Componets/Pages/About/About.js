1ac870a52b51439d3824c1dc7b769db671bb340e
PrinceTD <prince.talukder001@gmail.com>
2022-07-31 01:06:37 +0600

    added

--- src/Componets/Pages/About/About.js
+++ src/Componets/Pages/About/About.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import imgA from '../../../images/mukti.jpg'
+import imgA from '../../../images/images/anannya.jpg'
 import Footer from '../Footer/Footer'
 import Navbar from '../Header/Navbar'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function About() {
   return (
@@ -18,14 +20,26 @@ export default function About() {
 
                 <p> সাল ২০১৯</p>
                 <p>
-                  <small>
-                    সদ্য মফস্বল থেকে সিলেটে আসা স্বপ্নবাজ তরুণী অনন্যা  রায় তখন সিলেট সরকারি মহিলা কলেজ এর ছাত্রী। মে মাসে এইচএসসি পরীক্ষার পর হাতখরচের জন্য টিউশনির বিকল্প খুঁজতে গিয়েই যাত্রা শুরু- ‘অনন্যা’ র। যাত্রা শুরু করা বা স্বপ্ন দেখা অনেক সহজ! কিন্তু সেই স্বপ্ন কে বাঁচাতে এবং বাস্তব করতে প্রয়োজন কঠোর পরিশ্রম এবং ইচ্ছাশক্তি।
 
-                  </small>
+                  সদ্য মফস্বল থেকে সিলেটে আসা স্বপ্নবাজ তরুণী অনন্যা  রায় তখন সিলেট সরকারি মহিলা কলেজ এর ছাত্রী। মে মাসে এইচএসসি পরীক্ষার পর হাতখরচের জন্য টিউশনির বিকল্প খুঁজতে গিয়েই যাত্রা শুরু- ‘অনন্যা’ র। যাত্রা শুরু করা বা স্বপ্ন দেখা অনেক সহজ! কিন্তু সেই স্বপ্ন কে বাঁচাতে এবং বাস্তব করতে প্রয়োজন কঠোর পরিশ্রম এবং ইচ্ছাশক্তি।
+
+
                 </p>
                 <p>
-                  <small> সময় পেড়িয়ে অনন্যা এবং তার সেই ভালো বন্ধুদের পরিশ্রম এবং কাজের জন্য  ‘অনন্যার’র পণ্য আজ আপনার হাতে।</small>
+                  সময় পেড়িয়ে অনন্যা এবং তার সেই ভালো বন্ধুদের পরিশ্রম এবং কাজের জন্য  ‘অনন্যার’র পণ্য আজ আপনার হাতে।
                 </p>
+              </div>
+              <hr></hr>
+              <div>
+                <h5>My Socialmedia Account: </h5>
+                <span>
+                  <span className=' '>
+                    <a className='text-drak' href='https://www.instagram.com/_anannya_online_/' target="_blank"> <InstagramIcon fontSize="small"></InstagramIcon></a>
+                  </span>
+                  <span>
+                    <a className='text-drak' href='https://m.facebook.com/anannya.roy.376?tsid=0.8957403861757252&source=result' target="_blank"> <FacebookIcon fontSize="small" /></a>
+                  </span>
+                </span>
               </div>
             </div>
           </div>
